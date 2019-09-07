c85504a3a4906b3a2670ad30c6437498b4370996
Sattish Shewkani <51452387+Sattishh@users.noreply.github.com>
2019-09-07 17:24:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ client.on('message', message => {
         const exampleEmbed = new Discord.RichEmbed()
             .setColor('#800080')
             .setTitle('Help')
-            .setDescription('Here are the bot commands!')
+            .setDescription('Here are the bot commands! Use ```_help command name``` for help on usage. **Case Sensitive**')
             .setThumbnail('https://i.ibb.co/gMS6gX4/mono.png')
             .addBlankField()
             .addField('_EmX', 'Simply describes the bot')
             .addBlankField()
-            .addField('_invite', 'Gives you an invite link to bring the bot to your server', true)
+            .addField('_Invite', 'Gives you an invite link to bring the bot to your server', true)
             .addBlankField()
-            .addField('_tree', 'Plants a tree at will', true)
+            .addField('_Tree', 'Plants a tree at will', true)
             .addBlankField()
             .addField('_Dog', 'Sends a randomly generated picture of a doggo', true)
             .addBlankField()
@@ -38,9 +38,79 @@ client.on('message', message => {
             .setFooter('Bot created by Sattish#2011', 'https://i.ibb.co/gMS6gX4/mono.png');
         message.channel.sendMessage(exampleEmbed)
     }
+    if (message.content === '_help EmX') {
+        const EmXEmbed = new Discord.RichEmbed()
+        .setColor('#800080')
+        .setTitle('Help about EmX')
+        .setDescription('Describes the bot in simple words, thats it, do you really have to ask for this? :unamused:')
+        message.channel.sendMessage(EmXEmbed)
+    }
+    if (message.content === '_help Invite') {
+    const InviteEmbed = new Discord.RichEmbed()
+        .setColor('#800080')
+        .setTitle('Help about Invitation')
+        .setDescription('Bot sends a link which will allow you to bring the bot to your server!')
+        .addField('Aliases')
+        .addBlankField()
+        .addField('_Invite | _invite')
+        message.channel.sendMessage(InviteEmbed)
+    }
+        if (message.content === '_help Tree') {
+    const TreeEmbed = new Discord.RichEmbed()
+        .setColor('#800080')
+        .setTitle('Help on how to water a tree')
+        .setDescription('Are you really asking a bot for help on watering a tree? **Amateur**')
+        .addField('Aliases')
+        .addBlankField()
+        .addField('_Tree | _tree')
+        message.channel.sendMessage(TreeEmbed)
+    }
+        if (message.content === '_help Dog') {
+               
+    const DogEmbed = new Discord.RichEmbed()
+        .setColor('#800080')
+        .setTitle('Help about Doggos')
+        .setDescription('Bot sends a randomized picture of a doggo from the internet! If a picture shows up again, thats because its a randomized system and the bot could choose any photos at all!')
+        .addField('Aliases')
+        .addBlankField()
+        .addField('_Dog | _dog')
+    message.channel.sendMessage(DogEmbed)
+    }
+      if (message.content === '_help Cat') {
+               
+    const CatEmbed = new Discord.RichEmbed()
+        .setColor('#800080')
+        .setTitle('Help about Fluff Cats')
+        .setDescription('Bot sends a randomized picture of a cattos from the internet! If a picture shows up again, thats because its a randomized system and the bot could choose any photos at all!')
+        .addField('Aliases')
+        .addBlankField()
+        .addField('_Cat | _cat')
+    message.channel.sendMessage(CatEmbed)
+      }
+          
+           if (message.content === '_help Random') {
+               
+    const RandEmbed = new Discord.RichEmbed()
+        .setColor('#800080')
+        .setTitle('Help about Random pictures')
+        .setDescription('Bot sends a randomized picture from the internet! If a picture shows up again, thats because its a randomized system and the bot could choose any photos at all!')
+        .addField('Aliases')
+        .addBlankField()
+        .addField('_Random | _random | _Rand | _rand')
+    message.channel.sendMessage(RandEmbed)
+           }
     
     if (message.content === '_invite') {
-        message.channel.sendMessage(`https://discordapp.com/api/oauth2/authorize?client_id=612536352751353886&permissions=67584&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D612536352751353886%26permissions%3D8%26scope%3Dbot&scope=bot`)
+         if (message.content === '_help Cat') {
+               
+    const InvEmbed = new Discord.RichEmbed()
+        .setColor('#800080')
+        .setTitle('Heres the link!')
+        .setDescription('https://discordapp.com/api/oauth2/authorize?client_id=612536352751353886&permissions=67584&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D612536352751353886%26permissions%3D8%26scope%3Dbot&scope=bot')
+       
+    }
+      if (message.content === '_Invite') {
+       message.channel.sendMessage(InvEmbed)
     }
     function Cat8ball() {
         var rand = ['http://bit.ly/2k0cr31', 'http://bit.ly/2lwabAZ', 'http://bit.ly/2lw5pTU', 'http://bit.ly/2lw5DKK', 'http://bit.ly/2jURKW0', 'http://bit.ly/2kfOhlg', 'http://bit.ly/2kpdVUB', 'http://bit.ly/2jWmtlx', 'http://bit.ly/2lPAXnQ', 'http://bit.ly/2ltsu9Z', 'http://bit.ly/2lzSDDO', 'http://bit.ly/2lttRFF'];
