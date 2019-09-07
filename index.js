c65532b63ab0ee830c4194f8e04804002ba7d4e8
Karthikeyan Arumugam <kamtechie@gmail.com>
2019-09-07 17:38:36 +0100

    more code trimming

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-
-
 client.on("ready", () => {
     console.log("I am ready!");
     client.user.setStatus('dnd');
     client.user.setActivity('Work In Progress', { type: 2 });
     console.log(`Logged in as ${client.user.tag}!`);
-
-
-
 });
 
 client.on('message', message => {
@@ -91,7 +86,7 @@ client.on('message', message => {
             .addField('_Random | _random | _Rand | _rand')
         message.channel.sendMessage(RandEmbed)
     }
-    if (message.content === '_invite') {
+    if (message.content.toLowerCase() === '_invite') {
 
 
         const InvEmbed = new Discord.RichEmbed()
@@ -100,18 +95,12 @@ client.on('message', message => {
             .setDescription('https://discordapp.com/api/oauth2/authorize?client_id=612536352751353886&permissions=67584&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D612536352751353886%26permissions%3D8%26scope%3Dbot&scope=bot')
         message.channel.sendMessage(InvEmbed)
     }
-    if (message.content === '_Invite') {
-        message.channel.sendMessage(InvEmbed)
-    }
     function Cat8ball() {
         var rand = ['http://bit.ly/2k0cr31', 'http://bit.ly/2lwabAZ', 'http://bit.ly/2lw5pTU', 'http://bit.ly/2lw5DKK', 'http://bit.ly/2jURKW0', 'http://bit.ly/2kfOhlg', 'http://bit.ly/2kpdVUB', 'http://bit.ly/2jWmtlx', 'http://bit.ly/2lPAXnQ', 'http://bit.ly/2ltsu9Z', 'http://bit.ly/2lzSDDO', 'http://bit.ly/2lttRFF'];
 
         return rand[Math.floor(Math.random() * rand.length)];
     }
-    if (message.content === '_Cat') {
-        message.channel.sendMessage(Cat8ball())
-    }
-    if (message.content === '_cat') {
+    if (message.content.toLowerCase() === '_cat') {
         message.channel.sendMessage(Cat8ball())
     }
     function Dog8ball() {
@@ -119,10 +108,7 @@ client.on('message', message => {
         return james_charles[Math.floor(Math.random() * james_charles.length)];
     }
 
-    if (message.content === '_Dog') {
-        message.channel.sendMessage(Dog8ball())
-    }
-    if (message.content === '_dog') {
+    if (message.content.toLowerCase() === '_dog') {
         message.channel.sendMessage(Dog8ball())
     }
     function Random() {
