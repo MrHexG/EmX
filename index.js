0eb59310b85103035da43ecc3f8a11d1c6c70b97
Karthikeyan Arumugam <kamtechie@gmail.com>
2019-09-07 17:16:09 +0100

    testing with one embed

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ client.on("ready", () => {
     client.user.setActivity('Work In Progress', { type: 2 });
     console.log(`Logged in as ${client.user.tag}!`);
 
-    
-    
+
+
 });
 
 client.on('message', message => {
-   if (message.content ==='_EmX') {
+    if (message.content === '_EmX') {
         message.channel.sendMessage(`Hello, I'm a bot in progress right now but if you wish to add me to your server, that's fine! do _invite. You can also learn more about my commands by doing _help`);
     }
     if (message.content === '_help') {
@@ -38,16 +38,25 @@ client.on('message', message => {
             .setFooter('Bot created by Sattish#2011', 'https://i.ibb.co/gMS6gX4/mono.png');
         message.channel.sendMessage(exampleEmbed)
     }
-
+    if (message.content === '_help Invite') {
+        const InviteEmbed = new Discord.RichEmbed()
+            .setColor('#800080')
+            .setTitle('Help about Invitation')
+            .setDescription('Bot sends a link which will allow you to bring the bot to your server!')
+            .addField('Aliases')
+            .addBlankField()
+            .addField('_Invite | _invite')
+        message.channel.sendMessage(InviteEmbed)
+    }
     function Cat8ball() {
         var rand = ['http://bit.ly/2k0cr31', 'http://bit.ly/2lwabAZ', 'http://bit.ly/2lw5pTU', 'http://bit.ly/2lw5DKK', 'http://bit.ly/2jURKW0', 'http://bit.ly/2kfOhlg', 'http://bit.ly/2kpdVUB', 'http://bit.ly/2jWmtlx', 'http://bit.ly/2lPAXnQ', 'http://bit.ly/2ltsu9Z', 'http://bit.ly/2lzSDDO', 'http://bit.ly/2lttRFF'];
 
         return rand[Math.floor(Math.random() * rand.length)];
     }
-    if (message.content ==='_Cat') {
+    if (message.content === '_Cat') {
         message.channel.sendMessage(Cat8ball())
     }
-    if (message.content ==='_cat') {
+    if (message.content === '_cat') {
         message.channel.sendMessage(Cat8ball())
     }
     function Dog8ball() {
@@ -55,14 +64,14 @@ client.on('message', message => {
         return james_charles[Math.floor(Math.random() * james_charles.length)];
     }
 
-    if (message.content ==='_Dog') {
+    if (message.content === '_Dog') {
         message.channel.sendMessage(Dog8ball())
     }
     if (message.content === '_dog') {
         message.channel.sendMessage(Dog8ball())
     }
     function Random() {
-        var Random = ['http://bit.ly/2jZ2B1c', 'http://bit.ly/2khlyMY', 'http://bit.ly/2lwkfd8', 'http://bit.ly/2ktyorc', 'http://bit.ly/2k3ACO4', 'http://bit.ly/2kirdlV', 'http://bit.ly/2jYvV89', 'http://bit.ly/2ktWjab', 'http://bit.ly/2lXLgX9', 'http://bit.ly/2lwJ3BK', 'Fun Fact: This Cat beat my maker up! http://bit.ly/2ktz9Ay', 'http://bit.ly/2kqGEZb', 'http://bit.ly/2lXLgX9','http://bit.ly/2k24CK8'];
+        var Random = ['http://bit.ly/2jZ2B1c', 'http://bit.ly/2khlyMY', 'http://bit.ly/2lwkfd8', 'http://bit.ly/2ktyorc', 'http://bit.ly/2k3ACO4', 'http://bit.ly/2kirdlV', 'http://bit.ly/2jYvV89', 'http://bit.ly/2ktWjab', 'http://bit.ly/2lXLgX9', 'http://bit.ly/2lwJ3BK', 'Fun Fact: This Cat beat my maker up! http://bit.ly/2ktz9Ay', 'http://bit.ly/2kqGEZb', 'http://bit.ly/2lXLgX9', 'http://bit.ly/2k24CK8'];
         return Random[Math.floor(Math.random() * Random.length)];
     }
     if (message.content === '_random') {
@@ -72,17 +81,17 @@ client.on('message', message => {
         message.channel.sendMessage(Random())
     }
 
-    if (message.content ==='_rand') {
+    if (message.content === '_rand') {
         message.channel.sendMessage(Random())
     }
     if (message.content === '_Rand') {
         message.channel.sendMessage(Random())
     }
-        if (message.content === '_tree') {
+    if (message.content === '_tree') {
         message.channel.sendMessage(`A tree was watered! Thanks!`)
         message.channel.sendMessage(`https://tenor.com/view/clown-gif-10162552`)
     }
-        if (message.content === '_Tree') {
+    if (message.content === '_Tree') {
         message.channel.sendMessage(`A tree was watered! Thanks!`)
         message.channel.sendMessage(`https://tenor.com/view/clown-gif-10162552`)
     }
