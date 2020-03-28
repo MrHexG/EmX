7a0f4151a82f217bed1c6c386b853f0554fc1ca0
Sattishh <spshewkani@gmail.com>
2020-03-28 16:18:22 +0700

    forgot to add the ccoding for reaction_numbers smh

--- main.js
+++ main.js
@@ -47,6 +47,7 @@ var hangASCII = [
 ]
 var healthCode = ["0022FF", "00EFFF", "2BFF00", "F7FF00", "FF7700", "FF0000", "000000"]
 
+var reaction_numbers = ["\u0030\u20E3","\u0031\u20E3","\u0032\u20E3","\u0033\u20E3","\u0034\u20E3","\u0035\u20E3", "\u0036\u20E3","\u0037\u20E3","\u0038\u20E3","\u0039\u20E3"]
 
 var bm ="";
 var correctAnswer = "";
