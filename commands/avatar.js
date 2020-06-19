a2989448a90f9f9fce840a493b9b05aac7512fd2
Sattishh <spshewkani@gmail.com>
2020-06-19 19:44:19 +0700

    fixed avatar error

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,19 +2,22 @@ const Discord = require("discord.js");
 const botconfig = require("../files/botsettings.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.mentions.users.size) {
-        
-        return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
-        
-        }
-        const avatarList = message.mentions.users.map(user => {
-        
-        return `${user.username}\'s avatar: ${user.displayAvatarURL}`;
-        
-        });
-        
-    
-        message.channel.send(avatarList);
+    let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 }) : message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
+    if (message.mentions.users.size > 0) {
+      const embed = new Discord.MessageEmbed()
+        .setColor(0xFFFF00)
+        .setTitle(`Avatar for ${message.mentions.users.first().username}:`)
+        .setImage(`${avatar}`)
+        .setFooter('EmX by Sattish#2011');
+        message.channel.send({embed});
+    } else {
+      const embed = new Discord.MessageEmbed()
+      .setColor(0xFFFF00)
+      .setTitle(`Avatar for ${message.author.username}:`)
+      .setImage(`${avatar + "?size=2048"}`)
+      .setFooter('EmX by Sattish#2011');
+      message.channel.send({embed});
+    }
 }
 
 
