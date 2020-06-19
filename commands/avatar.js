33b76f76f309dd0062fa59ee0c620bef7fc58fe0
Sattishh <spshewkani@gmail.com>
2020-06-19 19:52:51 +0700

    Fixed avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,22 +2,9 @@ const Discord = require("discord.js");
 const botconfig = require("../files/botsettings.js");
 
 module.exports.run = async (bot, message, args) => {
-    let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 }) : message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
-    if (message.mentions.users.size > 0) {
-      const embed = new Discord.MessageEmbed()
-        .setColor(0xFFFF00)
-        .setTitle(`Avatar for ${message.mentions.users.first().username}:`)
-        .setImage(`${avatar}`)
-        .setFooter('EmX by Sattish#2011');
-        message.channel.send({embed});
-    } else {
-      const embed = new Discord.MessageEmbed()
-      .setColor(0xFFFF00)
-      .setTitle(`Avatar for ${message.author.username}:`)
-      .setImage(`${avatar + "?size=2048"}`)
-      .setFooter('EmX by Sattish#2011');
-      message.channel.send({embed});
-    }
+
+    message.channel.sendMessage(message.author.displayAvatarURL());
+
 }
 
 
